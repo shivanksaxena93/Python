4e635559e96166a024c70a0c7ceaec340e6789a6
Shivank Saxena <35405266+shivanksaxena93@users.noreply.github.com>
2019-03-26 17:19:42 -0700

    Add files via upload

---
+++ breakingALoop.py
@@ -0,0 +1,8 @@
+#Beaking a loop - Self Program
+for numbers in range (1, 20):
+	if numbers == 11:
+		break
+	if numbers >= 10:
+		print("Loop is about to get stopped") and print("Loop stopped")
+	else:
+		print(numbers)
